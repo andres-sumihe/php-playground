e194c801ae04a49d48f3c126425601c2aed95f5c
andres-sumihe <672018136@student.uksw.edu>
2020-09-30 20:32:09 +0700

    Solve Tugas 2 No 2

---
+++ Tugas2/No. 2a/halaman1.php
@@ -0,0 +1,5 @@
+<?php 
+	for ($i=1; $i <= 50 ; $i++) { 
+		echo ($i % 2 == 0) ?  "<font style='color: red;font-weight: bold;'>$i</font><br>" : "$i<br>";		
+	}
+ ?>
---
+++ Tugas2/No. 2b/halaman1.php
@@ -0,0 +1,5 @@
+<?php 
+	for ($i=25; $i <= 75 ; $i++) { 
+		echo ($i % 2 == 0) ? "$i<br>" : "<font style='color: blue;font-weight: bold;'>$i </font>" ;	
+	}
+ ?>
